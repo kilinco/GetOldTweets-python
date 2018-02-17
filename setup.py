79c31b762919c2b3de6f0a53aca0c056dec9b9da
Osman Cihan Kilinc <okilinc@ucsd.edu>
2018-02-16 22:37:47 -0800

    setup.py is added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+setup{
+	name = "GetOldTweets",
+	version = "1.0.0",
+	author = "Osman Cihan Kilinc",
+	author_email = "okilinc@ucsd.edu",
+	description = ("Forked from the original https://github.com/Jefferson-Henrique/GetOldTweets-python, GetOldTweets help "
+						"to get data from Twitter."),
+	license = "MIT",
+	keywords = "twitter",
+	url = "https://github.com/kilinco/GetOldTweets-python",
+	packages = ['got', 'got3'],
+	long_description = read('README'),
+	classifiers=[
+		"Development status :: 3 - Alpha",
+		"Topic :: Utilities",
+		"license :: MIT"
+	],
+	install_requires =["lxml", "pyquery"],
+}
